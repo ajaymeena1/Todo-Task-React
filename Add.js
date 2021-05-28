9a7ccc74deb294fd02f2f564fcc00039e2a92501
ajaymeena1 <55799216+ajaymeena1@users.noreply.github.com>
2021-05-28 17:57:43 +0530

    Add files via upload

---
+++ Add.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Todo.css';
+
+
+
+class Add extends Component {
+    render() {
+        return (
+            <div>
+                <ol>
+                    {this.props.ar.map((item, index) => (
+                        <li key={index}>
+                            {item}
+                            
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
+                                <label className="form-check-label" htmlFor="flexCheckChecked">
+                                    checkbox
+                                 </label>
+                            </div>
+                            <button onClick={this.props.deleteTask.bind(index)}>delete</button>
+                        
+                        </li>
+                    ))}
+                </ol>
+
+            </div>
+        )
+    }
+}
+
+export default Add
